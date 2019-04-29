23c8533c469e6376d409a009dc1743cb18ccd203
lessaeduarda <49791303+lessaeduarda@users.noreply.github.com>
2019-04-29 15:55:50 -0300

    Update lista_04_MariaEduarda_Lessa.R

--- lista_04_MariaEduarda_Lessa.R
+++ lista_04_MariaEduarda_Lessa.R
@@ -49,8 +49,8 @@ docentes_pe_selec <- docentes_pe %>% filter(NU_IDADE >= 18,
 
 # Checar filtro através das estatísticas descritivas da nova base: 
 summary(docentes_pe_selec$NU_IDADE)
-Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-18.00   32.00   39.00   39.96   48.00   70.00 
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 18.00   32.00   39.00   39.96   48.00   70.00 
 
 ## 2.3 - Não deve haver aluno com mais de 25 anos ou com menos de 1 ano;
 
@@ -63,8 +63,8 @@ matricula_pe_selec <- matricula_pe %>% filter(NU_IDADE >= 1,
 
 # Checar filtro através das estatísticas descritivas da nova base:
 summary(matricula_pe_selec$NU_IDADE)
-Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-1.00    8.00   12.00   11.63   15.00   25.00 
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 1.00    8.00   12.00   11.63   15.00   25.00 
 
 
 ## 2.4 - Apresente estatísticas descritivas do número de alunos por docente nos municípios do Estado;
@@ -87,57 +87,61 @@ matricula_por_docente <- inner_join(matricula_por_municipio, docentes_por_munici
 # Estatísticas descritivas da variável Mat_Doc (quarta coluna), da base "matricula_por_docente":
 summary(matricula_por_docente [,4])
   
-   Mat_Doc     
-Min.   :4.431  
-1st Qu.:5.464  
-Median :5.945  
-Mean   :6.043  
-3rd Qu.:6.584  
-Max.   :9.557
+#    Mat_Doc     
+# Min.   :4.431  
+# 1st Qu.:5.464  
+# Median :5.945  
+# Mean   :6.043  
+# 3rd Qu.:6.584  
+# Max.   :9.557
 
 
 ## 2.5 - Apresente o município com maior número de alunos por docente e seu IDHM;
 
 # Filtrar base do PNUD para resultados dos municípios de Pernambuco (UF 26) mais recentes (2010); selecionar apenas 2 variáveis da base original 
 # (IDHM e Codmun7); renomear variável (de Codmun7 para CO_MUNICIPIO); unir nova base à "matricula_por_docente", classificar a variável "Mat_Doc"
-# em ordem decrescente:
-dados_PNUD_pe <- dados_PNUD %>% filter(UF == 26, ANO == 2010
+# em ordem decrescente. Nomear nova base "aluno_docente_IDHM":
+aluno_docente_IDHM <- dados_PNUD %>% filter(UF == 26, ANO == 2010
                                        )%>% select(Codmun7,IDHM
                                                    ) %>% rename(CO_MUNICIPIO = Codmun7
                                                                 ) %>% inner_join(matricula_por_docente
                                                                                  ) %>% arrange(desc(Mat_Doc))
 
 # Checar primeiros resultados:
-head(dados_PNUD_pe)
+head(aluno_docente_IDHM)
 # A tibble: 6 x 5
-CO_MUNICIPIO  IDHM Mat_Muni Doc_Muni Mat_Doc
-   <dbl>      <dbl>    <int>    <int>   <dbl>
-1   2615805   0.519     6986      731    9.56  
-2   2615102   0.545     1882      210    8.96
-3   2609154   0.487     6391      714    8.95
-4   2607505   0.51      7277      894    8.14
-5   2607000   0.523     6138      772    7.95
-6   2606309   0.595     2082      269    7.74
-# Resultado: Município 2615805 (TUPANATINGA) é o que tem o maior número de alunos por docente e IDHM (em 2010) de 0.519.
+# CO_MUNICIPIO  IDHM Mat_Muni Doc_Muni Mat_Doc
+#    <dbl>      <dbl>    <int>    <int>   <dbl>
+# 1   2615805   0.519     6986      731    9.56  
+# 2   2615102   0.545     1882      210    8.96
+# 3   2609154   0.487     6391      714    8.95
+# 4   2607505   0.51      7277      894    8.14
+# 5   2607000   0.523     6138      772    7.95
+# 6   2606309   0.595     2082      269    7.74
+
+# Município 2615805 (TUPANATINGA) é o que tem o maior número de alunos por docente e IDHM (em 2010) de 0.519.
 
 ## 2.6 - Faça o teste do coeficiente de correlação linear de pearson e apresente sua resposta;
 
-# Atribuir "Aluno_por_Docente" à variável "Mat_Doc"(número de matrículas / número de docentes) da base "dados_PNUD_pe":
-Aluno_por_Docente <- dados_PNUD_pe$Mat_Doc
+# Atribuir "Aluno_por_Docente" à variável "Mat_Doc"(número de matrículas / número de docentes) da base "aluno_docente_IDHM":
+Aluno_por_Docente <- aluno_docente_IDHM$Mat_Doc
 
-# Atribuir "IDHM" à variável "IDHM" da base "dados_PNUD_pe":
-IDHM <- dados_PNUD_pe$IDHM
+# Atribuir "IDHM" à variável "IDHM" da base "aluno_docente_IDHM":
+IDHM <- aluno_docente_IDHM$IDHM
 
 # Calcular a correlação entre as duas variáveis:
 cor(Aluno_por_Docente,IDHM)
-[1] -0.5057435
+# [1] -0.5057435
 
 ## 2.7 - Seu script deve salvar a base de dados criada para o cálculo em formato .RData;
 
 # Salvar nova base em .RData:
-save(dados_PNUD_pe, file = "alunos_docentes_IDHM.RData")
+save(aluno_docente_IDHM, file = "alunos_docentes_IDHM.RData")
 
 ### Questão 3: 
 
+# Requerer pacote "ggplot2":
 require(ggplot2)
+
+# Usar função "plot" para gerar um scatterplot de alunos por docente (eixo x) e IDHM (eixo y):
 plot(Aluno_por_Docente,IDHM)
